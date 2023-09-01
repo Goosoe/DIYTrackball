fcd9bdf87b8fbed659ffef9e54d109d82ee96be5
Goosoe <miguel.al.lourenco@gmail.com>
2023-09-01 01:12:09 +0100

    cleanup

--- trackball.ino
+++ trackball.ino
@@ -40,10 +40,11 @@ Module   Arduino
 PMW3360 sensor;
 
 // Slave Select pin. Connect this to SS on the module.
-constexpr uint8_t selectionPin = 18;
-constexpr uint8_t button1 = 2;
-constexpr uint8_t button2 = 3;
+constexpr uint8_t SS_PIN = 18;
 constexpr uint8_t NUM_BUTTONS = 2;
+constexpr uint16_t CPI = 1000;
+constexpr uint8_t B1_PIN = 2;
+constexpr uint8_t B2_PIN = 3;
 PMW3360_DATA data;
 
 struct ButtonData
@@ -53,7 +54,7 @@ struct ButtonData
     bool previousInVal;
 };
 
-ButtonData buttons[NUM_BUTTONS] = {{button1, MOUSE_LEFT, false}, {button2, MOUSE_RIGHT, false}};
+ButtonData buttons[NUM_BUTTONS] = {{B1_PIN, MOUSE_LEFT, false}, {B2_PIN, MOUSE_RIGHT, false}};
 
 void readButtons()
 {
@@ -78,23 +79,18 @@ void readSensor()
 	sensor.readBurst(data);
     if(data.isOnSurface && data.isMotion)
 	{
-        Serial.print(data.dx);
-        Serial.print("\t");
-        Serial.print(data.dy);
-        Serial.println();
         Mouse.move(data.dx, -data.dy, 0);
 	}
 }
 
 void setup() 
 {
-	Serial.begin(9600);
-    pinMode(button1, INPUT_PULLUP);
-    pinMode(button2, INPUT_PULLUP);
+    pinMode(B1_PIN, INPUT_PULLUP);
+    pinMode(B2_PIN, INPUT_PULLUP);
   	Mouse.begin();
 
-  	sensor.begin(selectionPin);
-  //sensor.begin(10, 1600); // to set CPI (Count per Inch), pass it as the second parameter
+  //	sensor.begin(selectionPin);
+  sensor.begin(SS_PIN, CPI); // to set CPI (Count per Inch), pass it as the second parameter
 }
 
 void loop() {
