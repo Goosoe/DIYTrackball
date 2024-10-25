96aef651024b850d3a3adbe409bb15e98b720858
Goosoe <miguel.al.lourenco@gmail.com>
2024-10-25 13:27:57 +0100

    Invert encoder scroll direction

--- trackball.ino
+++ trackball.ino
@@ -130,8 +130,8 @@ void updateEncoder() {
   int encoded = (MSB << 1) | LSB; // Converting the 2 pin value to single number
   int sum  = (lastEncoded << 2) | encoded; // Adding it to the previous encoded value
 
-  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) scrollDir++;
-  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) scrollDir--;
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) scrollDir--;
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) scrollDir++;
   constrain(scrollDir, -SCROLL_SPEED_CLAMP, SCROLL_SPEED_CLAMP);
 
   lastEncoded = encoded; // Store this value for next time
